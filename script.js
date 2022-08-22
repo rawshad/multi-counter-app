f13a7fc8d76c905b09c0642b7721075540b67cdb
rawshad <rawshad@outlook.com>
2022-08-23 00:20:55 +0600

    updated

--- script.js
+++ script.js
@@ -34,3 +34,61 @@ const increment = (counterId, value) => {
 const decrement = (counterId, value) => {
 
 };
+
+//helper function
+function nextCounerId(counters) {
+    const maxId = counters.reduce(
+        (maxId, counter) => Math.max(counter.id, maxId),
+        -1
+    );
+    return maxId + 1;
+}
+
+//reducer function
+function counterReducer (state = initialState, action) {
+    if (action.type === ADD_COUNTER) {
+        return [
+            ...state,
+            {
+                id: nextCounerId(state),
+                value: 0,
+                incrementBy: Math.floor(Math.random() * 10) + 1,
+                decrementBy: Math.floor(Math.random() * 10) + 1,
+            }
+        ]
+    } if (action.type === RESET_COUNTERS) {
+        return state.map((counter) => ({
+            ...counter,
+            value: 0,
+        }));
+    } 
+    
+    if (action.type === INCREMENT) {
+        const {counterId, value} = action.payload;
+        return state.map((counter) => {
+            if (counter.id === counterId) {
+                return {
+                    ...counter,
+                    value: counter.value + value,
+                };
+            } 
+            return {
+                ...counter,
+            };
+        });
+    }
+    if (action.type === DECREMENT) {
+        const {counterId, value} = action.payload;
+        return state.map((counter) => {
+            if (counter.id === counterId) {
+                return {
+                    ...counter,
+                    value: counter.value - value,
+                };
+            } 
+            return {
+                ...counter,
+            };
+        });
+    }
+}
